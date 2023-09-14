f64d358ab4387655d22b242c9ccd162b32ed7e23
eterentiev <endthesame@gmail.com>
2023-09-14 12:26:52 +0300

    added rep etc and change ciphers

--- Lab1_Simple_Substitution_Cipher/atbash_code.py
+++ Lab1_Simple_Substitution_Cipher/atbash_code.py
@@ -16,7 +16,8 @@ def word_to_code(word, alphabet, codeAlphabet):
                 break
     return codeWord
 
-alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ']
+alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 
+            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ']
 atbashAlphabet = atbash_reverse(alphabet=alphabet)
 
 print(alphabet, "\n", atbashAlphabet)
